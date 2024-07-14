31d9107800bc5722bf07ca63db86e91833d1ab11
ShutaKimura <capybara1213@gmail.com>
2024-07-14 19:57:45 +0000

    SQL-controling
    Co-authored-by: Masato-929 <Masato-929@users.noreply.github.com>
    Co-authored-by: vimmer <tai-no-ala-ni@users.noreply.github.com>
    Co-authored-by: patapata <yusuke-hata-code@users.noreply.github.com>
    Co-authored-by: huhcat1 <huhcat1@users.noreply.github.com>
    Co-authored-by: KIMURA Shuta <k149017@kansai-u.ac.jp>
    Co-authored-by: yusei3150 <yusei3150@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mysql from "mysql";
-const app = express()
+const app = express();
 const port = 3000;
 const con =mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password:'kobakoba'//envにパスワードは移行することも検討
+    password:'kobakoba',//envにパスワードは移行することも検討
+    database: 'Attendanceclass'
 });
 
 con.connect(function(err){
     if (err) throw err;
     console.log(`Connected`);
-})
+});
+    // con.query('CREATE DATABASE express_db3', function (err, result) {
+    //     if (err) throw err; 
+    //       console.log(result);
+    //     });
+    // const sql = 'CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)';
+	// con.query(sql, function (err, result) {  
+	// if (err) throw err;  
+	// console.log('table created');  
+    // });
+    // const sql = "select * from users"
+	// con.query(sql, function (err, result, fields) {  
+	// if (err) throw err;  
+	// console.log(result)
+	// });
+    const sql = "INSERT INTO users(name,email) VALUES('kevin','kevin@test.com')"
+
+    con.query(sql,function(err, result, fields){
+        if (err) throw err;
+        console.log(result)
+    })
+    
+  app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+  });
+    app.get('/', (request, response) => {
+        const sql = "select * from users"
+        con.query(sql, function (err, result, fields) {  
+        if (err) throw err;
+        response.send(result)
+        });
+      });
+    
+
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
@@ -21,6 +55,5 @@ con.connect(function(err){
 //     console.log(req.body)
 //     res.send('Hello World!')    
 // })
-//  app.listen(port,()=>{
-//      console.log(`Example app listening on port ${port}`)
+ 
 //  })
